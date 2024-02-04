c8fdc6a34fa6208262a4d345206e2e7c04034251
Ignacio Rodríguez Blanco <ignaciorodriguezblanco97@gmail.com>
2024-02-04 20:47:42 +0100

    ejercicios finales

---
+++ T5Interfaces2/src/module-info.java
@@ -0,0 +1,12 @@
+module T5Interfaces2 {
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires javafx.base;
+	
+	opens ej1 to javafx.graphics, javafx.fxml;
+	opens ej2 to javafx.graphics, javafx.fxml;
+	opens ej3 to javafx.graphics, javafx.fxml;
+	opens ej4y5 to javafx.graphics, javafx.fxml;
+	opens ej6y7 to javafx.graphics, javafx.fxml;
+	
+}
